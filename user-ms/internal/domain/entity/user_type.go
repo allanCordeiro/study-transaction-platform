4a33faab2a83e70dc190d1605da67fab9bb7a386
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2024-02-18 18:26:41 -0300

    feat: add user type value object

--- user-ms/internal/domain/entity/user_type.go
+++ user-ms/internal/domain/entity/user_type.go
@@ -1,9 +1,22 @@
 package entity
 
-type UserType struct {
-	Value string
+type UserType uint8
+
+const (
+	Customer UserType = iota + 1
+	Distributor
+)
+
+func (u UserType) EnumIndex() uint8 {
+	return uint8(u)
 }
 
-func NewUserType(name string) *UserType {
-	return &UserType{Value: name}
+func (u UserType) String() string {
+	switch u {
+	case Customer:
+		return "customer"
+	case Distributor:
+		return "distributor"
+	}
+	return "unknow"
 }
