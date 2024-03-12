1edba6d69a82f6d2e354015910c2a225b67ed5b5
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2024-03-12 09:23:07 -0300

    feat: adiciona testes integracao para criar usuario no usecase

---
+++ user-ms/internal/usecases/create_user_it_test.go
@@ -0,0 +1,55 @@
+package usecases
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/AllanCordeiro/study-transaction-platform/user-ms/internal/database"
+	"github.com/AllanCordeiro/study-transaction-platform/user-ms/internal/domain/entity"
+	"github.com/AllanCordeiro/study-transaction-platform/user-ms/test"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var (
+	db              *mongo.Database
+	closeConnection func()
+	userDb          *database.UserDB
+)
+
+func TestMain(m *testing.M) {
+	//database, closeConnection := test.OpenConnection()
+	db, closeConnection = test.OpenConnection()
+
+	userDb = database.NewUserDB(db, "user")
+
+	defer closeConnection()
+
+	os.Exit(m.Run()) //you need to use it all the time or those tests wont pass
+}
+
+func TestCreateUser(t *testing.T) {
+	t.Run("given an user when create user usecase should return ok", func(t *testing.T) {
+		expectedName := "John Doe"
+		expectedEmail := "j.doe@test.com"
+		expectedType := "customer"
+		expectedPassword := "1233456"
+		expectedIsActive := true
+		newUser, err := entity.NewUser(expectedName, expectedEmail, expectedType, expectedPassword)
+
+		assert.Nil(t, err)
+		newUser.Activate()
+		err = userDb.Save(context.TODO(), newUser)
+		assert.Nil(t, err)
+
+		foundUser, err := userDb.FindByID(context.TODO(), newUser.Id)
+		assert.Nil(t, err)
+		assert.Equal(t, expectedName, foundUser.Name)
+		assert.Equal(t, expectedEmail, foundUser.Email.GetEmail())
+		assert.Equal(t, expectedType, foundUser.UserType.String())
+		assert.True(t, foundUser.IsPasswordValid(expectedPassword))
+		assert.Equal(t, expectedIsActive, foundUser.IsActive)
+
+	})
+}
--- user-ms/test/docker_test_connection.go
+++ user-ms/test/docker_test_connection.go
@@ -1,15 +1,43 @@
 package test
 
 import (
+	"context"
+	"fmt"
 	"log"
 
 	"github.com/ory/dockertest/v3"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func OpenConnection() (database *mongo.Database) {
-	_, err := dockertest.NewPool("")
+func OpenConnection() (database *mongo.Database, close func()) {
+	pool, err := dockertest.NewPool("")
 	if err != nil {
-		log.Fatalf("error")
+		log.Fatalf("Docker test error to build connection pool: %s", err)
+		return
 	}
+
+	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "mongo",
+		Tag:        "7-jammy",
+	})
+
+	if err != nil {
+		log.Fatalf("Erro do create mongo container: %s", err)
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(
+		fmt.Sprintf("mongodb://localhost:%s", resource.GetPort("27017/tcp"))))
+
+	if err != nil {
+		log.Fatalf("Error trying to open connection: %s", err)
+	}
+
+	return client.Database("user_test"), func() {
+		err := resource.Close()
+		if err != nil {
+			log.Println("Error trying to close connection")
+		}
+	}
+
 }
